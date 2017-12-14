e95c3d3103331eb236fbdf9876f4a53e47d1439e
Coder029 <andreitimofte29@gmail.com>
2017-12-14 10:44:31 +0200

    second commit

--- WindFormsAppDemoA/WindFormsAppDemoA/Form1.cs
+++ WindFormsAppDemoA/WindFormsAppDemoA/Form1.cs
@@ -16,5 +16,19 @@ namespace WindFormsAppDemoA
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*ksjfhdskjf shdkjfhsdk jfhsdkjf
+             lakdfjdas;lfkjas
+             \aasdas
+             das
+             d
+             sad
+             as
+             das
+             ff
+             gdsfsd;glfsdflkjsdlfkhsdfkjsdhfkjdsh*/
+        }
     }
 }
